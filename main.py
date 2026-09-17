439feecc673d60411801fc427f013d0d7f2ad202
Alexander Mikhailov <amihaylov@servicepipe.ru>
2026-09-18 01:15:55 +0500

    add task 1

---
+++ main.py
@@ -0,0 +1,72 @@
+import time
+
+
+def gcd_recursive(a: int, b: int) -> int:
+    """Вычисляет наибольший общий делитель двух целых чисел.
+    Рекурсивная реализация
+
+    :param a: целое число a
+    :param b: целое число b
+    :return: значение наибольшего общего делителя
+    """
+    pass
+
+
+def gcd_iterative_slow(a: int, b: int) -> int:
+    """Вычисляет наибольший общий делитель двух целых чисел.
+    Медленная итеративная реализация
+
+    :param a: целое число a
+    :param b: целое число b
+    :return: значение наибольшего общего делителя
+    """
+    pass
+
+
+def gcd_iterative_fast(a: int, b: int) -> int:
+    """Вычисляет наибольший общий делитель двух целых чисел.
+    Быстрая итеративная реализация
+
+    :param a: целое число a
+    :param b: целое число b
+    :return: значение наибольшего общего делителя
+    """
+    pass
+
+
+def lcm(a: int, b: int) -> int:
+    """Вычисляет наименьшее общее кратное двух натуральных чисел
+
+    :param a: натуральное число a
+    :param b: натуральное число b
+    :return: значение наименьшего общего кратного
+    """
+    pass
+
+
+def main():
+    a = 1005002
+    b = 1354
+    print(f"Вычисление НОД чисел {a} и {b} рекурсивно:")
+    start_time = time.time()
+    print(gcd_recursive(a, b))
+    print(f"Продолжительность: {time.time() - start_time} сек")
+
+    print(f"\nВычисление НОД чисел {a} и {b} итеративно с вычитанием:")
+    start_time = time.time()
+    print(gcd_iterative_slow(a, b))
+    print(f"Продолжительность: {time.time() - start_time} сек")
+
+    print(f"\nВычисление НОД чисел {a} и {b} итеративно с делением:")
+    start_time = time.time()
+    print(gcd_iterative_fast(a, b))
+    print(f"Продолжительность: {time.time() - start_time} сек")
+
+    print(f"\nВычисление НОК чисел {a} и {b}:")
+    start_time = time.time()
+    print(lcm(a, b))
+    print(f"Продолжительность: {time.time() - start_time} сек")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,94 @@
+import unittest
+from collections import namedtuple
+
+from gcd_generator import GcdGenerator
+from main import gcd_iterative_fast, gcd_iterative_slow, gcd_recursive, lcm
+
+Case = namedtuple("Case", ["a", "b", "result"])
+
+
+class TestGcd(unittest.TestCase):
+    """Набор тестов для проверки реализации функций расчета НОД.
+    Каждый тест запускается трижды, для каждой из реализаций расчета НОД"""
+
+    gcd_functions = (gcd_recursive, gcd_iterative_slow, gcd_iterative_fast)
+    simple_set = (
+        Case(9, 3, 3),
+        Case(-9, 3, 3),
+        Case(3, 5, 1),
+        Case(-18, -45, 9),
+        Case(1, 0, 1),
+        Case(0, 1, 1),
+    )
+    long_set = (
+        Case(1005002, 1354, 2),
+        Case(-1005002, 1354, 2),
+        Case(65225655, 1610510, 805255),
+        Case(-1944, -332024, 8),
+        Case(65225655, 0, 65225655),
+        Case(0, 65225655, 65225655),
+    )
+
+    def test_simple(self):
+        """Проверка вычисления НОД на примерах небольших чисел"""
+        for gcd in self.gcd_functions:
+            for case in self.simple_set:
+                with self.subTest(func=gcd.__name__, a=case.a, b=case.b):
+                    self.assertEqual(case.result, gcd(case.a, case.b))
+
+    def test_long(self):
+        """Проверка вычисления НОД на примерах относительно больших чисел"""
+        for gcd in self.gcd_functions:
+            for case in self.long_set:
+                with self.subTest(func=gcd.__name__, a=case.a, b=case.b):
+                    self.assertEqual(case.result, gcd(case.a, case.b))
+
+    def test_random(self):
+        """Проверка вычисления НОД на случайно сгенерированных примерах"""
+        gcd_gen = GcdGenerator()
+        if gcd_gen.gcd_value is None:
+            self.skipTest("GcdGenerator is not implemented")
+        for gcd in self.gcd_functions:
+            for i in range(10):
+                if gcd == gcd_iterative_fast:
+                    gcd_gen.generate_values(9, 5)
+                else:
+                    gcd_gen.generate_values(3, 2)
+                with self.subTest(
+                    func=gcd.__name__, i=i, a=gcd_gen.a_value, b=gcd_gen.b_value
+                ):
+                    self.assertEqual(
+                        gcd(gcd_gen.a_value, gcd_gen.b_value), gcd_gen.gcd_value
+                    )
+
+
+class TestLcm(unittest.TestCase):
+    """Набор тестов для проверки реализации функции расчета НОК"""
+
+    def test_primes(self):
+        """Проверка вычисления НОК для простых чисел"""
+        self.assertEqual(21, lcm(7, 3))
+
+    def test_simple(self):
+        """Проверка вычисления НОК для небольших чисел"""
+        self.assertEqual(24, lcm(6, 8))
+
+    def test_simple_long(self):
+        """Проверка вычисления НОК для относительно больших чисел"""
+        self.assertEqual(680386354, lcm(1005002, 1354))
+
+    def test_random(self):
+        """Проверка вычисления НОК на случайно сгенерированных примерах"""
+        gcd_gen = GcdGenerator()
+        if gcd_gen.gcd_value is None:
+            self.skipTest("GcdGenerator is not implemented")
+        for i in range(10):
+            gcd_gen.generate_values(4, 4)
+            with self.subTest(i=i, a=gcd_gen.a_value, b=gcd_gen.b_value):
+                self.assertEqual(
+                    lcm(gcd_gen.a_value, gcd_gen.b_value), gcd_gen.lcm_value
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
