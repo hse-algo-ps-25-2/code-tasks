d74388530420d7bdde141055c4cde432105703dd
Andrei Belskii <mrandrewer@yandex.ru>
2026-09-19 13:18:25 +0300

    Add gcd_iterative_slow implementation

--- main.py
+++ main.py
@@ -28,7 +28,15 @@ def gcd_iterative_slow(a: int, b: int) -> int:
     :param b: целое число b
     :return: значение наибольшего общего делителя
     """
-    pass
+    if a < 0:
+        a = abs(a)
+    if b < 0:
+        b = abs(b)
+    while a > 0 and b > 0:
+        if a < b:
+            a, b = b, a
+        a, b = b, a - b
+    return a + b
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
