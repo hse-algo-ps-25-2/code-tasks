564fb7adcc4a1b8480406997cea02e11aaa66765
Andrei Belskii <mrandrewer@yandex.ru>
2026-09-19 13:12:21 +0300

    Add gcd_recursive implementation

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def gcd_recursive(a: int, b: int) -> int:
     :param b: целое число b
     :return: значение наибольшего общего делителя
     """
-    pass
+    if a * b == 0:
+        return a + b
+    if a < 0:
+        a = abs(a)
+    if b < 0:
+        b = abs(b)
+    if a < b:
+        a, b = b, a
+    return gcd_recursive(b, a - b)
 
 
 def gcd_iterative_slow(a: int, b: int) -> int:
