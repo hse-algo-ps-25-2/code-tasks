a705f3ccead8f1a7e7124e9587753e83de117c83
Iaroslav Rodionov <iaroslav.java@gmail.com>
2026-09-23 19:31:05 +0300

    Implement GCD test data generator

--- gcd_generator.py
+++ gcd_generator.py
@@ -36,26 +36,30 @@ class GcdGenerator:
     @property
     def gcd_value(self) -> int:
         """Возвращает значение НОД для a_value и b_value"""
-        pass
+        return self.__values[COMMON_FACTORS]
 
     @property
     def a_value(self) -> int:
         """Возвращает число, полученное в результате перемножения простых чисел,
         возведенных в случайную степень. Число имеет как общие, так и различные
         множители с числом b_value"""
-        pass
+        return self.__values[COMMON_FACTORS] * self.__values[A_ONLY_FACTORS]
 
     @property
     def b_value(self) -> int:
         """Возвращает число, полученное в результате перемножения простых чисел,
         возведенных в случайную степень. Число имеет как общие, так и различные
         множители с числом a_value"""
-        pass
+        return self.__values[COMMON_FACTORS] * self.__values[B_ONLY_FACTORS]
 
     @property
     def lcm_value(self) -> int:
         """Возвращает значение НОК для a_value и b_value"""
-        pass
+        return (
+            self.__values[COMMON_FACTORS]
+            * self.__values[A_ONLY_FACTORS]
+            * self.__values[B_ONLY_FACTORS]
+        )
 
     @property
     def max_factor_cnt(self) -> int:
@@ -71,7 +75,14 @@ class GcdGenerator:
         умолчанию 5.
         :return: None
         """
-        pass
+        self.__values = [1, 1, 1]
+
+        primes = random.sample(self.__primes, factor_cnt)
+
+        for prime in primes:
+            factor = prime ** random.randint(1, max_pow)
+            group = random.randint(COMMON_FACTORS, B_ONLY_FACTORS)
+            self.__values[group] *= factor
 
 
 if __name__ == "__main__":
