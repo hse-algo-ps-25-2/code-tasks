af74532afa1f9e0cd3bb62932c6fbfc0a266ce9a
Zrossiz <soon.pluxury@mail.ru>
2026-09-23 18:04:25 +0300

    feat: add solution

---
+++ main.py
@@ -0,0 +1,60 @@
+from profilehooks import profile
+
+
+def fibonacci_rec(n: int) -> int:
+    """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
+    формуле вычисления последовательности.
+
+    :param n: порядковый номер числа Фибоначчи
+    :return: число Фибоначчи
+    """
+    if n in (1, 2):
+        return 1
+
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
+
+
+
+def fibonacci_iter(n: int) -> int:
+    """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
+    массива для хранения вычисляемых данных.
+
+    :param n: порядковый номер числа Фибоначчи
+    :return: число Фибоначчи
+    """
+    if n in (1, 2):
+        return 1
+
+    fibo_list = [1, 1]
+
+    for i in range(2, n): fibo_list.append(fibo_list[-1] + fibo_list[-2])
+
+    return fibo_list[-1]
+
+
+def fibonacci(n: int) -> int:
+    """Возвращает N-е число Фибоначчи. Реализована итеративно без использования массива.
+
+    :param n: Порядковый номер числа Фибоначчи
+    :return: число Фибоначчи
+    """
+    a, b = 1, 1
+    for _ in range(n - 1): a, b = b, a + b
+
+    return a
+
+
+def main():
+    n = 35
+    print(f"Вычисление {n} числа Фибоначчи рекурсивно:")
+    print(fibonacci_rec(n))
+
+    print(f"\nВычисление {n} числа Фибоначчи итеративно:")
+    print(fibonacci_iter(n))
+
+    print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
+    print(fibonacci(n))
+
+
+if __name__ == "__main__":
+    main()
