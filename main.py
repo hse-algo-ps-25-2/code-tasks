46ed7f903fc65f4eedfff2fd8f29ef512f577131
Ambrosovskaya Daria <t6lv6nksoxnk@mail.ru>
2026-09-25 22:19:39 +0400

    Решение Itera

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ def get_tridiagonal_determinant(matrix: list[list[int]]) -> int:
     :raises Exception: если matrix не является такой матрицей
     :return: значение определителя
     """
-    pass
+    n = len(matrix)
+
+    # порядок меньше 1 или не квадратная
+    if n == 0 or any(len(row) != n for row in matrix):
+        raise Exception()
+    # в каждой диагонали, кроме трёх, должны быть 0 - проверяем первую строку и первый столбец
+    if any(x != 0 for x in matrix[0][2:]) or any(row[0] != 0 for row in matrix[2:]):
+        raise Exception()
+
+    res = 0
+    res += matrix[0][0] * (1 if n == 1 else get_tridiagonal_determinant([row[1:] for row in matrix[1:]]))
+    if n > 1:
+        res -= matrix[0][1] * matrix[1][0] * (1 if n < 3 else get_tridiagonal_determinant([row[2:] for row in matrix[2:]]))
+
+    return res
 
 
 def main():
